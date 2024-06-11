9a1e24d7cca532b9f39dfad884f1d7506e7a7ac4
Symon <its.symonbarua@gmail.com.com>
2024-06-11 07:59:03 +0600

    Cart issue fixed

--- store/views.py
+++ store/views.py
@@ -20,7 +20,7 @@ def store(request, category_slug=None):
         product_count = products.count()
 
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
